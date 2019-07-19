6227d60621715a8e889a9e7f05cf61962ef4460c
Your Name <Matthewjdemeo@knights.ucf.edu>
2019-07-18 22:20:37 -0400

    Challenge

--- Platformer/Assets/Scripts/PlayerController.cs
+++ Platformer/Assets/Scripts/PlayerController.cs
@@ -1,17 +1,39 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
-    private Rigidbody2D rb2d;
     public float speed;
     public float jumpForce;
+    public Text countText;
+    public Text winText;
+    public Text livesText;
+    public Text loseText;
+    public AudioClip musicClipOne;
+    public AudioClip musicClipTwo;
+    public AudioSource musicSource;
+    private Rigidbody2D rb2d;
+    private int count;
+    private int lives;
+    
 
     // Start is called before the first frame update
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
+        count = 0;
+        lives = 3;
+        winText.text = "";
+        loseText.text = "";
+        countText.text = "";
+        livesText.text = "";
+        SetAllText ();
+
+        musicSource.clip = musicClipOne;
+        musicSource.Play();
+        musicSource.loop = true;
     }
 
     // Update is called once per frame
@@ -19,6 +41,39 @@ public class PlayerController : MonoBehaviour
     {
         if (Input.GetKey("escape"))
      Application.Quit();
+
+     if (Input.GetKeyDown(KeyCode.Space))
+    {
+        musicSource.clip = musicClipOne;
+        musicSource.Play();
+    }
+    if (Input.GetKeyUp(KeyCode.Space))
+    {
+        musicSource.Stop();
+    }
+    if (Input.GetKeyDown(KeyCode.P))
+    {
+        musicSource.clip = musicClipTwo;
+        musicSource.Play();
+    }
+    if (Input.GetKeyUp(KeyCode.P))
+    {
+        musicSource.Stop();
+    }
+    if (Input.GetKeyDown(KeyCode.L))
+    {
+        musicSource.loop = true;
+    }
+    if (Input.GetKeyUp(KeyCode.L))
+    {
+        musicSource.loop = false;
+    }
+    if (count >= 4)
+    {
+        musicSource.clip = musicClipTwo;
+        musicSource.Play();
+    }
+
     }
 
     void FixedUpdate()
@@ -39,6 +94,37 @@ public class PlayerController : MonoBehaviour
         }
     }
 
+void OnTriggerEnter2D(Collider2D other)
+{
+         if (other.gameObject.CompareTag("Pick Up"))
+         {
+             other.gameObject.SetActive(false);
+             count++; 
+             SetAllText();
+         }
+ 
+         else if (other.gameObject.CompareTag("Enemy"))
+         {
+             other.gameObject.SetActive(false);
+             count--; 
+             lives--;
+             SetAllText();
+         }
+     }
 
+void SetAllText()
+{
+    countText.text = "Score: " + count.ToString();
+    livesText.text = "Lives: " + lives.ToString();
+    if (count >= 4)
+    {  
+        winText.text = "You Win!";
+
+    }
+    else if (lives == 0)
+    {
+        loseText.text = "You Lose!";
+    }
+}
 
 }
